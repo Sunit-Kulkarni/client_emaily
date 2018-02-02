f85cb92dc58367cd8ed534f124a8beb54f51690d
Sunit Kulkarni <25267606+Sunit-Kulkarni@users.noreply.github.com>
2018-02-01 22:32:09 -0500

    added SurveyField component

---
+++ src/components/surveys/SurveyField.js
@@ -0,0 +1,13 @@
+//SurveyField contains logic to render a single
+//label and text input
+import React from 'react';
+
+export default props => {
+  console.log(props);
+
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
