ddfb911294296d510522fc181d432090228db4af
Sunit Kulkarni <25267606+Sunit-Kulkarni@users.noreply.github.com>
2018-02-06 01:58:59 -0500

    added reverse to the list of surveys rendered

--- src/components/surveys/SurveyList.js
+++ src/components/surveys/SurveyList.js
@@ -8,14 +8,14 @@ class SurveyList extends Component {
   }
 
   renderSurveys() {
-    return this.props.surveys.map(survey => {
+    return this.props.surveys.reverse().map(survey => {
       return (
         <div className="card darken-1" key={survey._id}>
           <div className="card-content">
             <span className="card-title">{survey.title}</span>
             <p>{survey.body}</p>
             <p className="right">
-              Sent On:{new Date(survey.dateSent).toLocaleDateString()}
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
             </p>
           </div>
           <div className="card-action">
