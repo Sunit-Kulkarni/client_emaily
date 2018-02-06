a52fd59b5b7296be7955de72338f695de6add1fb
Sunit Kulkarni <25267606+Sunit-Kulkarni@users.noreply.github.com>
2018-02-06 01:48:55 -0500

    added renderSurveys method

--- src/components/surveys/SurveyList.js
+++ src/components/surveys/SurveyList.js
@@ -6,8 +6,29 @@ class SurveyList extends Component {
   componentDidMount() {
     this.props.fetchSurveys();
   }
+
+  renderSurveys() {
+    return this.props.surveys.map(survey => {
+      return (
+        <div className="card darken-1" key={survey._id}>
+          <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p>{survey.body}</p>
+            <p className="right">
+              Sent On:{new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <a>Yes: {survey.yes}</a>
+            <a>No: {survey.no}</a>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>SurveyList</div>;
+    return <div>{this.renderSurveys()}</div>;
   }
 }
 
