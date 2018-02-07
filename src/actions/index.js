73deaab3f5d958f9fef682bb00d71f4b88e12176
Sunit Kulkarni <25267606+Sunit-Kulkarni@users.noreply.github.com>
2018-02-07 15:20:07 -0500

    made action creator for get request to /map/cities

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 export const fetchUser = () => async dispatch => {
   //action creator called
-  //redux thunk see return function an will have dispatch
+  //redux thunk see return function and will have dispatch
   const res = await axios.get('/api/current_user');
   dispatch({ type: FETCH_USER, payload: res.data });
   //wait until response gets back from api and only then will the action be dispatched
@@ -28,4 +28,10 @@ export const fetchSurveys = () => async dispatch => {
   const res = await axios.get('/api/surveys');
 
   dispatch({ type: FETCH_SURVEYS, payload: res.data });
+};
+
+export const getMap = () => async dispatch => {
+  const res = await axios.get('/map/cities');
+
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
